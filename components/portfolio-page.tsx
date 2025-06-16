bc861692ad7d31ce525d8cba3f219bc3becf5ac4
Atishay8192261 <smartatishay09@gmail.com>
2025-06-16 02:34:23 -0700

    deplyment error still

--- components/portfolio-page.tsx
+++ components/portfolio-page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Navbar } from "@/components/Navbar"
+import { Navbar } from "./Navbar"
 import { useState, useEffect } from "react"
 import { motion } from "framer-motion"
 import { Profile } from "./Profile"
